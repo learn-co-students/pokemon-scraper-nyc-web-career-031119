c8b29b0ebb45d2f00b7ec9fc62aa0f8284271e9b
J-Agens <johntagens@gmail.com>
2019-03-23 13:11:50 -0400

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -1,7 +1,8 @@
 require_relative "environment"
-
+require 'pry'
 Scraper.new(@db).scrape
 
 all_pokemon = @db.execute("SELECT * FROM pokemon;")
-
+binding.pry
 # test out your code here!
+puts "last line"
--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,41 @@
+require 'pry'
 class Pokemon
+  attr_accessor :id, :name, :type, :db, :hp
+
+  def initialize(id: nil, name: name, type: type, db: @db, hp: 60)
+    @name = name
+    @type = type
+  end
+
+  def self.save(name, type, db)
+    new_pokemon = Pokemon.new
+    new_pokemon.name = name
+    new_pokemon.type = type
+    sql = <<-SQL
+      INSERT INTO pokemon (name, type)
+      VALUES (?, ?)
+    SQL
+    db.execute(sql, new_pokemon.name, new_pokemon.type)
+    new_pokemon.id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+  end
+
+  def self.new_from_db(row)
+    new_pokemon = Pokemon.new
+    new_pokemon.name = row[1]
+    new_pokemon.type = row[2]
+    new_pokemon.id = row[0]
+    new_pokemon.hp = row[3]
+    new_pokemon
+  end
+
+  def self.find(id, db)
+    sql = "SELECT * FROM pokemon WHERE id = ?"
+    result = db.execute(sql, id)[0]
+    self.new_from_db(result)
+  end
+
+  def alter_hp(hp, db)
+    sql = "UPDATE pokemon SET hp = ? WHERE name = ?"
+    db.execute(sql, hp, self.name)
+  end
 end
